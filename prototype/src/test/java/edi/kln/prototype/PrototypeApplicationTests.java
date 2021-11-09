c1dc59b117b05832524bf72236054770214360a0
CheahZZ <cheahzz@kerrylogistics.online>
2021-11-09 15:34:43 +0800

    Project Git init and init proto package

---
+++ prototype/src/test/java/edi/kln/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package edi.kln.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
