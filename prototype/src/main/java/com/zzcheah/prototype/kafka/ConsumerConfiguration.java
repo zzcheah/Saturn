0905be7398769f37637ccef0a44464fbe8b9b6ac
CheahZZ <cheahzz@kerrylogistics.online>
2021-12-09 23:18:05 +0800

    feat(client): mock kafka consumer
    #20

--- prototype/src/main/java/com/zzcheah/prototype/kafka/ConsumerConfiguration.java
+++ prototype/src/main/java/com/zzcheah/prototype/kafka/ConsumerConfiguration.java
@@ -1,19 +1,22 @@
 package com.zzcheah.prototype.kafka;
 
-import com.zzcheah.common_base.CommonBaseClass;
+import com.zzcheah.common_base.ReceiverOutput;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
 @Configuration
+@Slf4j
 public class ConsumerConfiguration {
 
     @Bean
-    public Consumer<CommonBaseClass> consumer() {
+    public Consumer<ReceiverOutput> consumer() {
         return payload -> {
             System.out.println("do something from kafka");
-            payload.doSomething();
+            log.info(payload.toString());
+
         };
     }
 }
